776cbb01c64605cfddf86bce22ee34abe7cff0dc
Zhenzhen You <swimreally@MacBook-Air.local>
2018-04-04 08:07:38 -0700

    add key in button row, add dot and equal conditions in calculation

--- src/calculation.js
+++ src/calculation.js
@@ -1,54 +1,79 @@
 function calculation(value, state) {
   if (value === 'C' || value === 'AC') {
+    return {
+      result: ''
+    };
+  }
+
+  let result = state.concat(value);
+  
+  if (value === '.') {
+    if (state[state.length - 1] === '.') {
       return {
-        result: ''
+        result: state
       };
     }
+    return {
+      result: result
+    };
+  }
 
   let num1;
   let num2;
   let finalNum;
-  let result = state.concat(value);
 
   for (let i = 0; i < result.length; i++) {
 
     if ( (isNaN(parseFloat(result[i]))) 
-      && (result[i] !== ".") 
-      && (i !== result.length - 1) ) {
+      && (i !== result.length - 1)
+      && (result[i] !== '.') ) {
 
       let operator = result[i];
+      let finalOperator = result[result.length - 1];
       num1 = parseFloat(result.slice(0, i));
       num2 = parseFloat(result.slice(i + 1, result.length - 1));
 
-      if (operator === "+") {
+      if (operator === '=') {
+        finalNum = num2;
+      }
+
+      if (operator === '+') {
         finalNum = num1 + num2;
       }
 
-      else if (operator === "-") {
+      else if (operator === '-') {
         finalNum = num1 - num2;
       }
 
-      else if (operator === "÷") {
-        finalNum = (num1 / num2).toFixed(2);
+      else if (operator === '÷') {
+        finalNum = Math.round( (num1 / num2) * 100 ) / 100;
       }
 
-      else if (operator === "x") {
+      else if (operator === 'x') {
         finalNum = num1 * num2;
       }
 
-      else if (operator === "%") {
+      else if (operator === '%') {
         finalNum = num1 % num2;
       }
 
-      else if (operator === "^")  {
+      else if (operator === '^')  {
         finalNum = Math.pow(num1, num2);
       }
 
-      state = finalNum.toString();
+      if (finalOperator === '=') {
+        state = finalNum.toString();
+        return {
+          result: state
+        };
+      }
 
-      return {
-        result: finalNum.toString()
-      };
+      else {
+        state = finalNum.toString().concat(finalOperator);
+        return {
+          result: state
+        };
+      } 
     }
   }
 
